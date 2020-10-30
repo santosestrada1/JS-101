ddc768e33f8bc136ab1417e4264b932d40bb4d84
Santos Estrada <santos.estrada1@gmail.com>
2020-10-30 10:31:02 -0500

    Friday js 101 2nd push

--- 101-exercises.js
+++ 101-exercises.js
@@ -13,10 +13,31 @@ const assert = function(actual, expected, message = "") {
 
 // Keep this function here in order to add correct questions to the counter
 function addToDone(message) {
-    const node = document.createElement("LI");
-    const textnode = document.createTextNode(message);
-    node.appendChild(textnode);
+    const node = document.createElement("LI");              // Create a <li> node
+    const textnode = document.createTextNode(message);              // Create a text node
+    node.appendChild(textnode);                                    // Append the text to <li>
     node.classList.add("finished");
     document.querySelector(".correct ul").appendChild(node);
     document.querySelector("#count").innerHTML = document.querySelectorAll('.finished').length + " of 101";
 }
+
+// Welcome to 101 Exercises in JS
+// If you get "ReferenceError: someVariableName is not defined", that means you need to create a variable
+
+// Exercise #0, Example Problem:
+// Example problem setup: Create a variable named doingJSRightNow and assign it the boolean true.
+// The line below creates the variable named doingJSRightNow and assigns the boolean value true.
+// To complete Exercise #0, uncomment the following line of JS.
+let doingJSRightnow = document.getElementById(true);
+
+// The lines below will test your answer. If you see an error, then it means that your answer is incorrect or incomplete.
+let doingJSRightnow = (true);
+
+
+// Exersise 1
+// On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
+let onMarsRightNow = false;
+assert(onMarsRightNow, false, "Exercise 1");
+addToDone("Exercise 1 is correct.");
+
+// Exercise 2
