2be15dadaeca824cbe34cdf6abd21ce515a7a399
Santos Estrada <santos.estrada1@gmail.com>
2021-02-04 10:07:03 -0600

    completed exercise #9

--- 101-exercises.js
+++ 101-exercises.js
@@ -90,3 +90,7 @@ assert(vegetables, ["broccoli", "carrot", "cauliflower",  "eggplant", "tomato", 
 addToDone("Exercise 8 is correct.")
 
 // Exercise 9
+// Write the code necessary to sort the fruits in reverse alphabetical order
+fruits.sort();fruits.reverse();
+assert(fruits, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana'], "Exercise 9")
+addToDone("Exercise 9 is correct.")
