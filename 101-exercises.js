0c87d72dadc4be85d4aa918346fd13027066843e
Santos Estrada <santos.estrada1@gmail.com>
2021-02-08 12:55:08 -0600

    Completed Exercise #10

--- 101-exercises.js
+++ 101-exercises.js
@@ -94,3 +94,13 @@ addToDone("Exercise 8 is correct.")
 fruits.sort();fruits.reverse();
 assert(fruits, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana'], "Exercise 9")
 addToDone("Exercise 9 is correct.")
+
+// Exercise 10
+// Write the code necessary to produce a single array that holds all fruits then all vegetables in the order as they were sorted above.
+// Assign the result to a variable named fruitsAndVeggies.
+// *hint* the search engine search here would be "how to combine two arrays in JavaScript", for example.
+
+var fruitsAndVeggies = fruits.concat(vegetables)
+assert(fruitsAndVeggies, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana', 'broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini'], "Exercise 10")
+addToDone("Exercise 10 is correct")
+
